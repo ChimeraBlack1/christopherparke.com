44ea8af4a8b685ad11386110d25ea85a238d8806
chimerablack <christopherparke@hotmail.com>
2016-11-17 13:03:24 -0500

    added bootgal project to project section

---
+++ bootgal/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/normalize.css">
+      <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+      <header class="header">
+          <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Link1</a></li>
+                <li><a href="#">Link2</a></li>
+            </ul>
+          </nav>
+      </header>
+      
+      
+      <div class='content-wrap'>
+        <h1>Random Gallery</h1>
+        <div class='container'>
+            <ul class="row">
+                <li class='col-sm-4'></li>
+                <li class='col-sm-4'></li>
+                <li class='col-sm-4'></li>
+            </ul>
+        </div>
+      </div>
+      <div class="container">
+         <div class="row">
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+         </div>
+          <div class="row">
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+         </div>
+          <div class="row">
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+         </div>
+          <div class="row">
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+         </div>
+          <div class="row">
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+           <div class="col-sm-4">
+               <div class="gallery">
+                One of three columns
+               </div>
+           </div>
+         </div>
+       </div>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  </body>
+</html>
